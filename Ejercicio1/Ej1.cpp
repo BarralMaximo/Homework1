2eb8f5d56276a287aa8962166eb60118a11b8885
BarralMaximo <barralm@udesa.edu.ar>
2025-03-15 16:55:43 -0300

    Fixed error in exercise one when input is invalid, defined n as 0 to avoid entering the function with an unknown value

--- Ejercicio1/Ej1.cpp
+++ Ejercicio1/Ej1.cpp
@@ -2,30 +2,41 @@
 #include <vector>
 
 using namespace std;
-typedef vector<vector<int>> matrix;
+using matrix = vector<vector<int>>;
 
+/**
+ * Crea una matriz cuadrada de tamaño n x n y la llena con valores secuenciales.
+ *
+ * @param n Tamaño de la matriz (n x n).
+ * @return Matriz cuadrada de tamaño n x n llena con valores secuenciales.
+ */
 matrix create_matrix(int n) {
-    matrix matrix(n, vector<int>(n));
+    matrix M(n, vector<int>(n));
     int cont = 1;
 
     for(int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matrix[i][j] = cont;
+            M[i][j] = cont;
             cont++;
         }
     }
-    return matrix;
+    return M;
 }
 
-void print_matrix(matrix matrix){
-    size_t size = matrix.size();
+/**
+ * Imprime una matriz cuadrada en orden inverso (de abajo hacia arriba y de derecha a izquierda).
+ *
+ * @param M Matriz cuadrada a imprimir.
+ */
+void print_matrix(matrix M){
+    size_t size = M.size();
     int row = size;
     int column = size - 1;
 
     for(int i = (size*size); i != 0; i--) {
         if (i % size == 0){row -= 1; column = size -1;}
         
-        cout << "M[" << row << "][" << column << "] = " << matrix[row][column] << endl;
+        cout << "M[" << row << "][" << column << "] = " << M[row][column] << endl;
 
         column -= 1;
     }
@@ -33,7 +44,7 @@ void print_matrix(matrix matrix){
 
 
 int main(){
-    int n;
+    int n = 0;
 
     cout << "Ejercicio 1 - Creación e impresión de matriz" << endl << "Por favor inserte un valor n: ";
 
